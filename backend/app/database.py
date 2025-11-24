f073c1a6c353a710756f9c0f61e8e66a3e977a6f
Hanma-yujiro <mpethamotaung@gmail.com>
2025-11-24 18:20:27 +0200

    Centralizes the ORM setup

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,2 +1,7 @@
 #Centralizes the ORM setup, including the base class
 # and session management for database interactions.
+
+from sqlalchemy.orm import declarative_base
+
+# The base class which all ORM models will inherit from.
+Base = declarative_base()
--- backend/app/models.py
+++
@@ -1,1 +0,0 @@
-#Define data schema
