3de6a82727068a544bccc8b33586f6b7853e7bda
Hanma-yujiro <mpethamotaung@gmail.com>
2025-11-28 16:53:21 +0200

    Frontend setup, create mock Dashboard and Page

---
+++ frontend/.next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"P+cVKN5lzokNyu4z7Gu2C1OLZFPv5G3ESiVoWeOA8qY=\"\n}"
---
+++ frontend/app/Dashboard.tsx
@@ -0,0 +1,10 @@
+// frontend/app/page.tsx
+import Dashboard from './Dashboard';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-100 p-8">
+      <Dashboard />
+    </main>
+  );
+}
